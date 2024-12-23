01e168bd4357aa528312b5f5729193a142a56901
TinoMaster <ommallono@gmail.com>
2024-12-23 12:46:10 +0100

    some fixes

--- src/pages/private/Private.routes.tsx
+++ src/pages/private/Private.routes.tsx
@@ -1,12 +1,14 @@
 import { Navigate, Route } from "react-router-dom";
 import { RoutesWithNotFound } from "../../components/common";
-import { Dashboard } from "../private";
+import { lazy } from "react";
+
+const DashBoardPage = lazy(() => import("../private/dashboard/Dashboard"));
 
 export const PrivateRoutes = () => {
   return (
     <RoutesWithNotFound>
       <Route path="/" element={<Navigate to="/dashboard" replace />} />
-      <Route path="dashboard" element={<Dashboard />} />
+      <Route path="dashboard" element={<DashBoardPage />} />
       <Route path="*" element={<Navigate to="/404" replace />} />
     </RoutesWithNotFound>
   );
--- src/pages/private/dashboard/Dashboard.tsx
+++ src/pages/private/dashboard/Dashboard.tsx
@@ -1,9 +1,11 @@
 import { Box, Typography } from "@mui/material";
 
-export const Dashboard = () => {
+const Dashboard = () => {
   return (
     <Box>
       <Typography variant="h5">Dashboard</Typography>
     </Box>
   );
-}
+};
+
+export default Dashboard;
